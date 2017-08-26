ee8380c27334d5208063edc55b523028f5243d58
Nathan Feaver <nathan.feaver@gmail.com>
2017-08-26 09:18:52 -0600

    clean ups

--- lib/flourish_web/router.ex
+++ lib/flourish_web/router.ex
@@ -14,13 +14,8 @@ defmodule FlourishWeb.Router do
   end
 
   scope "/", FlourishWeb do
-    pipe_through :browser # Use the default browser stack
+    pipe_through :browser
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", FlourishWeb do
-  #   pipe_through :api
-  # end
 end
