a10d526330fe6bb3ae6ec6d5ee07fdde8a380a9b
Nathan Feaver <nathan.feaver@gmail.com>
2017-09-26 14:48:08 -0600

    prepare for Heroku deploy

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :flourish, FlourishWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "flourish-nf.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -66,7 +68,11 @@ config :logger, level: :info
 
 # Claim-based request authentication
 config :flourish, Flourish.Authentication,
-  secret_key: %{
-    "k" => System.get_env("JWT_OCT_KEY"),
-    "kty" => "oct"
-  }
+  secret_key: Map.fetch!(System.get_env(), "AUTH_SECRET_KEY")
+
+# Configure your database
+config :flourish, Flourish.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
