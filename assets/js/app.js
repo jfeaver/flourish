1b2e0f948882d766f75698b59fbb7affc0bd471e
Nathan Feaver <nathan.feaver@gmail.com>
2017-10-03 13:02:37 -0600

    add cloudinary js assets

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,8 @@
 //
 // If you no longer want to use a dependency, remember
 // to also remove its path from "config.paths.watched".
+import "cloudinary-jquery-file-upload"
+import "jquery"
 import "phoenix_html"
 
 // Import local files
