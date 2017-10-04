df25712e06bd6baa470662e227612a3efaa7ba88
Nathan Feaver <nathan.feaver@gmail.com>
2017-10-04 13:13:26 -0600

    show a call to action to sign in

--- lib/flourish_web/auth_error_handler.ex
+++ lib/flourish_web/auth_error_handler.ex
@@ -1,5 +1,8 @@
 defmodule FlourishWeb.AuthErrorHandler do
+  import Phoenix.Controller
+
   def auth_error(conn, _info, _opts) do
     conn
+    |> put_flash(:info, "Your session has expired. Please log in again.")
   end
 end
