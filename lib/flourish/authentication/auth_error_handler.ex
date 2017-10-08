72dfe43af172537e351acd04a8046d0c2917465e
Nathan Feaver <nathan.feaver@gmail.com>
2017-10-08 16:14:17 -0600

    go to sign in on expired authentication

--- lib/flourish/authentication/auth_error_handler.ex
+++ lib/flourish/authentication/auth_error_handler.ex
@@ -1,8 +1,7 @@
 defmodule Flourish.Authentication.AuthErrorHandler do
   import Plug.Conn
 
-  def auth_error(conn, {type, _reason}, _opts) do
-    body = Poison.encode!(%{message: to_string(type)})
-    send_resp(conn, 401, body)
+  def auth_error(conn, {_type, _reason}, _opts) do
+    conn
   end
 end
